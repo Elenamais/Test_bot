e4e0521a5dba78deb060424bf941e29286c133f4
Elena Bespalova <82591792+Elenamais@users.noreply.github.com>
2024-04-08 17:52:18 +0200

    test
    test

---
+++ test_bot/main.py
@@ -0,0 +1,62 @@
+from pydub import AudioSegment
+import requests
+import telebot
+import cv2
+import numpy as np
+from io import BytesIO
+
+token = '7123175906:AAGq0YTE8Nhlw-dmQZ_vWsk2AIkLPJl5gaI'
+bot = telebot.TeleBot(token)
+
+audio_messages = {}
+
+# Функция для сохранения голосовых сообщений
+def save_voice_message(message):
+    user_id = message.from_user.id
+    file_id = message.voice.file_id
+
+    if user_id not in audio_messages:
+        audio_messages[user_id] = []
+
+    file = bot.get_file(file_id)
+    file_url = f"https://api.telegram.org/file/bot{token}/{file.file_path}"
+    response = requests.get(file_url)
+
+    audio = AudioSegment.from_file(BytesIO(response.content), format="ogg")
+    path = f"voice_messages/{user_id}_{len(audio_messages[user_id])}.wav"
+    audio.export(path, format="wav", parameters=["-ar", "16000"])
+
+    audio_messages[user_id].append(path)
+
+    bot.reply_to(message, "Голосовое сообщение сконвертировано в WAV и сохранено в папку voice_messages")
+
+# Обработчик для голосовых сообщений
+@bot.message_handler(content_types=['voice'])
+def handle_voice_message(message):
+    save_voice_message(message)
+
+# Функция для обнаружения лиц на фотографии
+def detect_faces_in_photo(image_bytes):
+    image = cv2.imdecode(np.frombuffer(image_bytes, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    return len(faces) > 0
+
+# Обработчик для фотографий
+@bot.message_handler(content_types=['photo'])
+def handle_photo_message(message):
+    file_id = message.photo[-1].file_id
+    file = bot.get_file(file_id)
+    file_url = f"https://api.telegram.org/file/bot{token}/{file.file_path}"
+    response = requests.get(file_url)
+
+    if detect_faces_in_photo(response.content):
+        file_path = f"photo_with_faces/photo_{file_id}.jpg"
+        with open(file_path, 'wb') as image_file:
+            image_file.write(response.content)
+        bot.send_message(message.chat.id, "Фото сохранено в папку photo_with_face")
+    else:
+        bot.send_message(message.chat.id, "На фото не обнаружены лица")
+
+if __name__ == '__main__':
+    bot.infinity_polling()
